fa3385999b2c54ad1177cfca174b38e09bead384
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-08-08 11:36:36 +0800

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -4,7 +4,7 @@ const apps = require('../data/apps.json');
 
 [apps, games].flat().forEach((app) => {
   if (app.icon) app.icon = `static/apps-images/${app.icon}`;
-  app.screenshots = app.screenshots.map((fileName) => `https://cdn.jsdelivr.net/gh/ChenYFan-Tester/appstore@gh-pages/master/static/apps-images/${fileName}`);
+  app.screenshots = app.screenshots.map((fileName) => `https://cdn.jsdelivr.net/gh/ChenYFan-Tester/appstore@gh-pages/static/apps-images/${fileName}`);
   app.screenshotsThumbs = app.screenshots;
 });
 
