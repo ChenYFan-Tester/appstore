c56fc3bfab655f2506ad460b1b4209e2577a3d5a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-08 02:37:13 +0000

    Deploy ChenYFan-Tester/appstore to github.com/ChenYFan-Tester/appstore.git:gh-pages

---
+++ js/404.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{54:function(t,n,e){"use strict";e.r(n);var s={},o=e(10),a=Object(o.a)(s,(function(){var t=this.$createElement,n=this._self._c||t;return n("f7-page",[n("f7-navbar",{attrs:{title:"Not found","back-link":"Back"}}),this._v(" "),n("f7-block",{attrs:{strong:""}},[n("p",[this._v("Sorry")]),this._v(" "),n("p",[this._v("Requested content not found.")])])],1)}),[],!1,null,null,null);n.default=a.exports}}]);
+//# sourceMappingURL=404.js.map
